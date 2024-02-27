982f23a10c9cc14f402b3d52fd0734c728c53b4a
Lukasz Michalik <l.michalik004@gmail.com>
2024-02-27 23:59:34 +0100

    work with chatbot

--- chat-bot/database.py
+++ chat-bot/database.py
@@ -20,37 +20,37 @@ def create_table():
     score INT)
     """)
   
-def format_data(data):
-  data = data.replace('\n', ' newlinechar ').replace('\r', ' newlinechar ').replace('"', "'")
-  return data
+# def format_data(data):
+#   data = data.replace('\n', ' newlinechar ').replace('\r', ' newlinechar ').replace('"', "'")
+#   return data
 
-def find_parent(pid):
-  try:
-    sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
-    c.execute(sql)
-    result = c.fetchone()
-    if result != None:
-      return result[0]
-    else:
-      return False
-  except Exception as e:
-    return False  
+# def find_parent(pid):
+#   try:
+#     sql = "SELECT comment FROM parent_reply WHERE comment_id = '{}' LIMIT 1".format(pid)
+#     c.execute(sql)
+#     result = c.fetchone()
+#     if result != None:
+#       return result[0]
+#     else:
+#       return False
+#   except Exception as e:
+#     return False  
 
 if __name__ == '__main__':
   create_table()
-  row_counter = 0
-  paired_rows = 0
+  # row_counter = 0
+  # paired_rows = 0
 
-  with open("~/Desktop/AI-Development/chat-bot/RC_{}-05.json".format(timeframe.split('-')[0], timeframe), 'rb') as f:
-    for row in f:
-      row_counter += 1
-      row = json.loads(row)
-      parent_id = row['parent_id']
-      body = format_data(row['body'])
-      created_utc = row['created_utc']
-      score = row['score']
-      subreddit = row['subreddit']
-      parent_data = find_parent(parent_id)
+  # with open("~/Desktop/AI-Development/chat-bot/RC_{}-05.json".format(timeframe.split('-')[0], timeframe), 'rb') as f:
+  #   for row in f:
+  #     row_counter += 1
+  #     row = json.loads(row)
+  #     parent_id = row['parent_id']
+  #     body = format_data(row['body'])
+  #     created_utc = row['created_utc']
+  #     score = row['score']
+  #     subreddit = row['subreddit']
+  #     parent_data = find_parent(parent_id)
 
       # if score >= 2:
       #   if acceptable(body):
